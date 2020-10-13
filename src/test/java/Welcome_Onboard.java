3cda3b299b8432f8106c8174baab06df182e774c
ALI <yildirimali@hotmail.co.uk>
2020-10-13 19:58:58 +0100

    Group2_Welcome_class

---
+++ src/test/java/Welcome_Onboard.java
@@ -0,0 +1,14 @@
+public class Welcome_Onboard {
+
+  //Hello boys and Grils wellcome on board..
+  //it is tiem to do some exercise in Github
+  //please sit back and relax
+  //Thank you for choosing National Express :-)
+
+
+
+
+
+
+
+}
